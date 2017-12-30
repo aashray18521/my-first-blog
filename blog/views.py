603b9a6fa708ed35e4228246b204eba3d16777cc
aashray18521 <aashray18521@gmail.com>
2017-12-30 21:21:21 +0530

    Improving HTML

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-	return render(request, 'blog/post_list.html')
+	posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+	#   parameter, {}, is a place in which we can add some things for the template to use.
+	return render(request, 'blog/post_list.html', {'posts': posts})
