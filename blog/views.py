e47562f04da49d7059da07795b2969cbb8d05f84
aashray18521 <aashray18521@gmail.com>
2017-12-30 22:45:34 +0530

    Updated HTML and added post_detail hyperlink

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -7,3 +7,7 @@ def post_list(request):
 	posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('-published_date')
 	#   parameter, {}, is a place in which we can add some things for the template to use.
 	return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk = pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
